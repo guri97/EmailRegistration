0364994a7a21a0e243fde8164266f9186f0a24a4
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-27 21:52:45 +0530

    [gurpreet] EmailRegistration

---
+++ EmailRegistration/EmailRegistration/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RegexPattern
+{
+    class Progran
+    {
+        public static void Main(string[] args)
+        {
+            RegexClass userRegexTest = new RegexClass();
+            String EmailAddress = "hdshreegowri@gmail.com";
+
+            string[] EmailList = { "abc@yohoo.com","abc-100@yohoo.com",
+                                    "abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net",
+                                     "abc.100@abc.com.au", "abc@1.com","abc","abc@.com.my",
+                                    "abc123@gmail.a",
+                                     "abc1232.com"
+                                     ,".abc@abc.com","abc123.com.my", "abc123@.com.com"};
+
+            userRegexTest.ValidateEmailAddressRegex(EmailAddress);
+            foreach (string SampleEmailAddress in EmailList)
+            {
+                userRegexTest.ValidateEmailAddressRegex(SampleEmailAddress);
+            }
+
+        }
+    }
+}
---
+++ EmailRegistration/EmailRegistration/RegexClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPattern
+{
+    public class RegexClass
+    {
+        public Regex EmailAddressRegex = new Regex(@"^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,})?$");
+        public void ValidateEmailAddressRegex(string EmailAddress)
+        {
+            Console.WriteLine("\nemail address:" + EmailAddress);
+            if (EmailAddressRegex.IsMatch(EmailAddress))
+                Console.WriteLine("valid email address");
+            else
+                Console.WriteLine("invalid email address");
+
+        }
+
+
+    }
+
+
+
+}
